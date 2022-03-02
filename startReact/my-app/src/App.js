1d7f97e366399b3705f9c766971840f052d09005
Shin <cjs4022@gmail.com>
2022-03-02 17:59:51 +0900

    [Update] React 학습 정리 시작

---
+++ startReact/my-app/src/App.js
@@ -0,0 +1,12 @@
+import Dice from './Dice';
+import Rock from './HandIcon';
+function App() {
+  return (
+    <div>
+      <Dice />
+      <Rock />
+    </div>
+  );
+}
+
+export default App;
---
+++ startReact/my-app/src/HandIcon.js
@@ -0,0 +1,7 @@
+import rock from "./assets/rock.svg"
+
+function handleIcon() {
+  return <img src = {rock} alt="주먹 이미지"/>;
+}
+
+export default handleIcon;
